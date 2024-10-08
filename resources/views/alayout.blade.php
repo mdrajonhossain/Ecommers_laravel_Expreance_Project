37e7bb8c90b9b831ab684d2b0bc5f9e3ad211448
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-10-08 15:09:13 +0600

    teassdf

--- resources/views/alayout.blade.php
+++ resources/views/alayout.blade.php
@@ -29,7 +29,7 @@
     <div class="md:flex">
         <div class="bg-sky-900 text-white sidebar md:w-[250px] p-4 hidden md:block">
             <ul>
-                <li class="p-2 cursor-pointer" style="font-size:14px;">
+                <li class="cursor-pointer" style="font-size:14px;">
                     <div class="flex justify-between items-center hover:bg-[#0369a1] p-2 submenu-toggle">
                         <div class="flex items-center space-x-2">
                             <i class="fas fa-home"></i>
@@ -39,7 +39,7 @@
                 </li>
 
 
-                <li class="p-2 cursor-pointer" style="font-size:14px;">
+                <li class="cursor-pointer" style="font-size:14px;">
                     <div class="flex justify-between items-center hover:bg-[#0369a1] p-2 submenu-toggle">
                         <div class="flex items-center space-x-2">
                             <i class="fa-solid fa-layer-group"></i>
@@ -65,11 +65,11 @@
 
 
                 
-                <li class="p-2 cursor-pointer" style="font-size:14px;">
+                <li class="cursor-pointer" style="font-size:14px;">
                     <div class="flex justify-between items-center hover:bg-[#0369a1] p-2 submenu-toggle">
                         <div class="flex items-center space-x-2">
                             <i class="fa-solid fa-sitemap"></i>
-                            <a href="#" class="text-white">Items</a>
+                            <a href="#" class="text-white">Products</a>
                         </div>
                         <span class="arrow text-white">&#10148;</span>
                     </div>
