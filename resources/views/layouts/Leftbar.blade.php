a46396b8e2b87cd6269212ffc89aa85d0e260483
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-10-14 18:18:32 +0600

    asdfasdf

--- resources/views/layouts/Leftbar.blade.php
+++ resources/views/layouts/Leftbar.blade.php
@@ -4,7 +4,7 @@
                     <div class="flex justify-between items-center hover:bg-[#0369a1] p-2 submenu-toggle">
                         <div class="flex items-center space-x-2">
                             <i class="fas fa-home"></i>
-                            <a href="#" class="text-white"> {{ $message }}</a>
+                            <a href="{{ url('/') }}" class="text-white"> {{ $message }}</a>
                         </div>
                     </div>
                 </li>
@@ -14,13 +14,13 @@
                     <div class="flex justify-between items-center hover:bg-[#0369a1] p-2 submenu-toggle">
                         <div class="flex items-center space-x-2">
                             <i class="fa-solid fa-layer-group"></i>
-                            <a href="#" class="text-white">Catagory</a>
+                            <a class="text-white">Catagory</a>
                         </div>
                         <span class="arrow text-white">&#10148;</span>
                     </div>
                     <ul class="submenu hidden pl-6 mt-2 space-y-2">
                         <li>
-                            <a href="#" class="block hover:bg-[#0369a1] p-2 flex items-center space-x-2">
+                            <a href="{{ url('/dashboard') }}" class="block hover:bg-[#0369a1] p-2 flex items-center space-x-2">
                                 <i class="fas fa-list"></i>
                                 <span>Catagory List</span>
                             </a>
