e0b54529f471b5756a23f8479af29bb8ce39c906
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-10-15 14:42:37 +0600

    asdfasdf

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,32 @@
-<div class="bg-sky-700 w-screen h-16 flex text-white">
-    Topbar
+<div class="bg-sky-700 w-screen text-white">
+    <div class="p-2 flex justify-between h-16 items-center gap-10">
+
+        <div class="flex items-center">
+            <img class="rounded"
+                src="https://img.freepik.com/premium-vector/bird-logo-design-ai-generated_1043906-6005.jpg" width="50px"
+                alt="Logo">
+            <div class="text-xl font-bold ml-3">BDShop</div>
+        </div>
+
+        <div class="flex-grow mx-4"></div>
+
+        <div class="flex-grow mx-4">
+            <div class="flex items-center bg-green-800 rounded-lg overflow-hidden">
+                <input type="text" placeholder="Search..."
+                    class="flex-grow p-2 outline-none text-gray-700 placeholder-gray-400">
+                <button class="p-2 bg-green-700 hover:bg-green-600 text-white rounded-r-lg">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <div class="flex items-center">
+            <div class="ml-4 flex items-center">
+                <button class="p-2 text-white"><i class="fas fa-add"></i></button>
+                <button class="p-2 text-white"><i class="fas fa-add"></i></button>
+                <button class="p-2 text-white"><i class="fas fa-add"></i></button>
+            </div>
+        </div>
+
+    </div>
 </div>
