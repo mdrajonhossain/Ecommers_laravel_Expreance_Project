358ae22737e69e27b43d7d9e67bd1590c24e43f0
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-10-15 17:34:44 +0600

    asdfasdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,54 +14,54 @@
 <div class="md:flex md:flex-wrap gap-6">
 
 
-    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-teal-700 p-7 text-white rounded">
+    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-teal-700 p-7 text-white rounded cursor-pointer">
         <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
         <div class="flex-[4]">Product</div>
         <div class="flex-1">10</div>
     </div>
 
 
-    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-teal-500 p-7 text-white rounded">
+    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-teal-500 p-7 text-white rounded cursor-pointer">
         <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
         <div class="flex-[4]">Product</div>
         <div class="flex-1">10</div>
     </div>
 
 
-    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-blue-500 p-7 text-white rounded">
+    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-blue-500 p-7 text-white rounded cursor-pointer">
         <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
         <div class="flex-[4]">Product</div>
         <div class="flex-1">10</div>
     </div>
 
-    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-yellow-500 p-7 text-white rounded">
+    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-yellow-500 p-7 text-white rounded cursor-pointer">
         <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
         <div class="flex-[4]">Product</div>
         <div class="flex-1">10</div>
     </div>
 
-    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-green-500 p-7 text-white rounded">
-        <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
-        <div class="flex-[4]">Product</div>
-        <div class="flex-1">10</div>
-    </div>
-
-
-    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-blue-800 p-7 text-white rounded">
+    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-green-500 p-7 text-white rounded cursor-pointer">
         <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
         <div class="flex-[4]">Product</div>
         <div class="flex-1">10</div>
     </div>
 
 
-    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-blue-800 p-7 text-white rounded">
+    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-blue-800 p-7 text-white rounded cursor-pointer">
         <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
         <div class="flex-[4]">Product</div>
         <div class="flex-1">10</div>
     </div>
 
 
-    <div class="mb-5 flex md:w-1/4 lg:w-1/4 bg-green-800 p-7 text-white rounded">
+    <div class="mb-5 md:mb-0 flex md:w-1/4 lg:w-1/4 bg-blue-800 p-7 text-white rounded cursor-pointer">
+        <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
+        <div class="flex-[4]">Product</div>
+        <div class="flex-1">10</div>
+    </div>
+
+
+    <div class="mb-5 flex md:w-1/4 lg:w-1/4 bg-green-800 p-7 text-white rounded cursor-pointer">
         <div class="flex-1"><i class="fa-solid fa-layer-group text-2xl"></i></div>
         <div class="flex-[4]">Product</div>
         <div class="flex-1">10</div>
