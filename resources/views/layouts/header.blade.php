920510d69a9b04e4f468c36872d127991f0be33e
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-10-15 16:23:55 +0600

    asdfasdf

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,7 @@
             <div class="flex items-center bg-green-800 rounded-lg overflow-hidden">
                 <input type="text" placeholder="Search..."
                     class="flex-grow p-2 outline-none text-gray-700 placeholder-gray-400">
-                <button class="pr-5 pl-5 pt-2 pb-2 bg-green-700 hover:bg-green-600 text-white rounded-r-lg">
+                <button class="pr-5 pl-5 pt-2 pb-2 bg-green-700 hover:bg-sky-600 text-white rounded-r-lg">
                     <i class="fas fa-search"></i>
                 </button>
             </div>
@@ -22,14 +22,14 @@
             <div class="ml-4 flex items-center gap-2">
 
                 <!-- heart Icon -->
-                <button class="p-2 text-white relative">
+                <button class="p-2 text-white relative hover:bg-sky-600">
                     <i class="fas fa-heart"></i>
                     <span class="absolute -top-1 -right-1 text-xs bg-red-600 text-white rounded-full px-1">3</span>
                     <!-- Badge for item count -->
                 </button>
 
                 <!-- Card Icon -->
-                <button class="p-2 text-white relative">
+                <button class="p-2 text-white relative hover:bg-sky-600">
                     <i class="fas fa-shopping-cart"></i>
                     <span class="absolute -top-1 -right-1 text-xs bg-red-600 text-white rounded-full px-1">3</span>
                     <!-- Badge for item count -->
@@ -38,7 +38,7 @@
 
 
                 <!-- Logout Button -->
-                <button id="logout-button" class="p-2 text-white relative">
+                <button id="logout-button" class="p-2 text-white relative hover:bg-sky-600">
                     <i class="fas fa-sign-out-alt"></i>
                 </button>
 
