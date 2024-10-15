d8a4d5c994c8d2770803a8ecffbaa17f67743acb
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-10-15 14:45:29 +0600

    asdfasdf

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,7 @@
             <div class="flex items-center bg-green-800 rounded-lg overflow-hidden">
                 <input type="text" placeholder="Search..."
                     class="flex-grow p-2 outline-none text-gray-700 placeholder-gray-400">
-                <button class="p-2 bg-green-700 hover:bg-green-600 text-white rounded-r-lg">
+                <button class="pr-5 pl-5 pt-2 pb-2 bg-green-700 hover:bg-green-600 text-white rounded-r-lg">
                     <i class="fas fa-search"></i>
                 </button>
             </div>
@@ -24,7 +24,7 @@
             <div class="ml-4 flex items-center">
                 <button class="p-2 text-white"><i class="fas fa-add"></i></button>
                 <button class="p-2 text-white"><i class="fas fa-add"></i></button>
-                <button class="p-2 text-white"><i class="fas fa-add"></i></button>
+                <button class="p-2 text-white"><i class="fas fa-sign-out-alt"></i></button>
             </div>
         </div>
 
